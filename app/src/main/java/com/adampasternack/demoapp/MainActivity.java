ad9649cdf40e70a8059e619cad7fd177e724609c
Adam Pasternack <adam.pasternack@huntington.com>
2017-04-26 22:20:53 -0400

    first button with click event

--- app/src/main/java/com/adampasternack/demoapp/MainActivity.java
+++ app/src/main/java/com/adampasternack/demoapp/MainActivity.java
@@ -2,8 +2,17 @@ package com.adampasternack.demoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+
+        Log.i("Info", "Button Pressed");
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
