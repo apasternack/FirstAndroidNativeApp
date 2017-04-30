0413fa3651e4811807fa265d78688b9bff39a5e3
Adam Pasternack <adam.pasternack@huntington.com>
2017-04-29 23:59:45 -0400

    toaster fun

--- app/src/main/java/com/adampasternack/demoapp/MainActivity.java
+++ app/src/main/java/com/adampasternack/demoapp/MainActivity.java
@@ -4,12 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view) {
 
-        Log.i("Info", "Button Pressed Mate!");
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+
+        Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+
+        Log.i("Info", myTextField.getText().toString());
 
 
     }
